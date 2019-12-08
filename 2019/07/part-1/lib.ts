5367188787774b4be369431b7c1ed0e387c94b52
Jo Hanna Pearce <jo@nrwl.io>
2019-12-08 14:54:37 +0000

    refactor: tidying up intCode

--- 2019/07/part-1/lib.ts
+++ 2019/07/part-1/lib.ts
@@ -16,13 +16,12 @@ export enum ParameterMode {
 }
 
 const isImmediate = (mode: ParameterMode) => mode === ParameterMode.Immediate;
-const readValue = (
-  mode: ParameterMode,
-  program: number[],
-  pointer: number
-): number => (isImmediate(mode) ? program[pointer] : program[program[pointer]]);
-const writeValue = (program: number[], pointer: number, value: number) => {
-  program[program[pointer]] = value;
+const readValue = (state: ProgramState, param: number): number =>
+  isImmediate(state.modes[param - 1])
+    ? state.program[state.pointer + param]
+    : state.program[state.program[state.pointer + param]];
+const writeValue = (state: ProgramState, param: number, value: number) => {
+  state.program[state.program[state.pointer + param]] = value;
 };
 
 export interface ProgramState {
@@ -37,54 +36,54 @@ export const opCodeFunctions: {
   [code in OpCode]: (state: ProgramState) => void;
 } = {
   [OpCode.Add]: (state: ProgramState) => {
-    const input1 = readValue(state.modes[0], state.program, state.pointer + 1);
-    const input2 = readValue(state.modes[1], state.program, state.pointer + 2);
-    writeValue(state.program, state.pointer + 3, input1 + input2);
+    const input1 = readValue(state, 1);
+    const input2 = readValue(state, 2);
+    writeValue(state, 3, input1 + input2);
     state.pointer += 4;
   },
 
   [OpCode.Multiply]: (state: ProgramState) => {
-    const input1 = readValue(state.modes[0], state.program, state.pointer + 1);
-    const input2 = readValue(state.modes[1], state.program, state.pointer + 2);
-    writeValue(state.program, state.pointer + 3, input1 * input2);
+    const input1 = readValue(state, 1);
+    const input2 = readValue(state, 2);
+    writeValue(state, 3, input1 * input2);
     state.pointer += 4;
   },
 
   [OpCode.Input]: (state: ProgramState) => {
     const input = state.input.pop();
-    writeValue(state.program, state.pointer + 1, input);
+    writeValue(state, 1, input);
     state.pointer += 2;
   },
 
   [OpCode.Output]: (state: ProgramState) => {
-    const value = readValue(state.modes[0], state.program, state.pointer + 1);
+    const value = readValue(state, 1);
     state.output.push(value);
     state.pointer += 2;
   },
 
   [OpCode.JumpIfTrue]: (state: ProgramState) => {
-    const input1 = readValue(state.modes[0], state.program, state.pointer + 1);
-    const input2 = readValue(state.modes[1], state.program, state.pointer + 2);
+    const input1 = readValue(state, 1);
+    const input2 = readValue(state, 2);
     state.pointer = input1 === 0 ? state.pointer + 3 : input2;
   },
 
   [OpCode.JumpIfFalse]: (state: ProgramState) => {
-    const input1 = readValue(state.modes[0], state.program, state.pointer + 1);
-    const input2 = readValue(state.modes[1], state.program, state.pointer + 2);
+    const input1 = readValue(state, 1);
+    const input2 = readValue(state, 2);
     state.pointer = input1 === 0 ? input2 : state.pointer + 3;
   },
 
   [OpCode.LessThan]: (state: ProgramState) => {
-    const input1 = readValue(state.modes[0], state.program, state.pointer + 1);
-    const input2 = readValue(state.modes[1], state.program, state.pointer + 2);
-    writeValue(state.program, state.pointer + 3, input1 < input2 ? 1 : 0);
+    const input1 = readValue(state, 1);
+    const input2 = readValue(state, 2);
+    writeValue(state, 3, input1 < input2 ? 1 : 0);
     state.pointer += 4;
   },
 
   [OpCode.Equals]: (state: ProgramState) => {
-    const input1 = readValue(state.modes[0], state.program, state.pointer + 1);
-    const input2 = readValue(state.modes[1], state.program, state.pointer + 2);
-    writeValue(state.program, state.pointer + 3, input1 === input2 ? 1 : 0);
+    const input1 = readValue(state, 1);
+    const input2 = readValue(state, 2);
+    writeValue(state, 3, input1 === input2 ? 1 : 0);
     state.pointer += 4;
   },
 
