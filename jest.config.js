df5a12b018b47042d79f140e9b03bf8a734b9c59
Jo Hanna Pearce <jo@nrwl.io>
2019-12-18 19:31:02 +0000

    chore: update jest config to ignore the template folder

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node"
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/2019/template', '<rootDir>/node_modules/']
 };
