3085f67a60a9b9c64c4c41af0999d116e7e1aad4
Jo Hanna Pearce <jo@nrwl.io>
2020-12-05 09:39:54 +0000

    feat(2020): day 5, Binary Boarding, part 2

--- 2020/05/part-2/lib.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('2020-12-05.2', () => {
-  const input = ``;
-
-  it('should  do a thing', () => {});
-});
--- 2020/05/part-2/main.ts
+++ 2020/05/part-2/main.ts
@@ -1,4 +1,32 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { getSeat, getSeatId } from '../part-1/lib';
 
 const input = fs.readFileSync(path.join(__dirname, '../input.txt')).toString();
+const passes = input.split('\n').filter((x) => x); // eliminate empty rows
+
+const seatMap = new Set();
+passes.forEach((pass) => {
+  const seat = getSeat(pass);
+  seatMap.add(getSeatId(seat));
+});
+
+const possibles = [];
+for (let row = 1; row < 127; row++) {
+  for (let seat = 0; seat < 8; seat++) {
+    const seatId = getSeatId({ row, seat });
+    if (!seatMap.has(seatId)) {
+      possibles.push(seatId);
+    }
+  }
+}
+
+// Look for the first discontinuity
+for (let i = 1; i < possibles.length; i++) {
+  if (possibles[i] === possibles[i - 1] + 1) {
+    continue;
+  }
+
+  console.log(`Seat Id is ${possibles[i]}`);
+  break;
+}
