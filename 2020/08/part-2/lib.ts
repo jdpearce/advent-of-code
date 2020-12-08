d8e8d99bbcd63325d0c6a88d4d902579ba233aa3
Jo Hanna Pearce <jo@nrwl.io>
2020-12-08 09:11:50 +0000

    chore(2020): mini refactor

--- 2020/08/part-2/lib.ts
+++ 2020/08/part-2/lib.ts
@@ -3,15 +3,14 @@ import { runCode } from '../part-1/lib';
 export function findTerminatingChange(input: string): number {
   const instructions = input.split('\n').filter((i) => i);
 
-  let i = 0;
-  for (; i < instructions.length; i++) {
+  for (let i = 0; i < instructions.length; i++) {
     const [op, value] = instructions[i].split(' ');
     if (op === 'acc') {
       continue;
     }
 
     let modified = [...instructions];
-    modified.splice(i, 1, `${op === 'nop' ? 'jmp' : 'nop'} ${value}`);
+    modified[i] = `${op === 'nop' ? 'jmp' : 'nop'} ${value}`;
 
     const result = runCode(modified.join('\n'));
     if (result.terminated) {
