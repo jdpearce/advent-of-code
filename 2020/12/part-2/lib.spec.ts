f6b2eb36985314d69a633e847c89ee3057733010
Jo Hanna Pearce <jo@nrwl.io>
2020-12-12 09:39:23 +0000

    feat(2020): day 12, Rain Risk, part 2 (better)

--- 2020/12/part-2/lib.spec.ts
+++ 2020/12/part-2/lib.spec.ts
@@ -1,5 +1,24 @@
-describe('2020-12-12.2', () => {
-  const input = ``;
+import { calculateManhattanDistance, Position, processInstruction } from './lib';
 
-  it('should  do a thing', () => {});
+describe('2020-12-12.2', () => {
+  it('should perform actions properly', () => {
+    const start: Position = { ship: { x: 0, y: 0 }, waypoint: { x: 10, y: 1 } };
+
+    let actual = processInstruction('F10', start);
+    expect(actual).toEqual({ ship: { x: 100, y: 10 }, waypoint: { x: 10, y: 1 } });
+
+    actual = processInstruction('N3', actual);
+    expect(actual).toEqual({ ship: { x: 100, y: 10 }, waypoint: { x: 10, y: 4 } });
+
+    actual = processInstruction('F7', actual);
+    expect(actual).toEqual({ ship: { x: 170, y: 38 }, waypoint: { x: 10, y: 4 } });
+
+    actual = processInstruction('R90', actual);
+    expect(actual).toEqual({ ship: { x: 170, y: 38 }, waypoint: { x: 4, y: -10 } });
+
+    actual = processInstruction('F11', actual);
+    expect(actual).toEqual({ ship: { x: 214, y: -72 }, waypoint: { x: 4, y: -10 } });
+
+    expect(calculateManhattanDistance(actual)).toBe(286);
+  });
 });
--- 2020/12/part-2/main.ts
+++ 2020/12/part-2/main.ts
@@ -1,4 +1,15 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { calculateManhattanDistance, Position, processInstruction } from './lib';
 
 const input = fs.readFileSync(path.join(__dirname, '../input.txt')).toString();
+const instructions = input.split('\n').filter((x) => x);
+
+let position: Position = { ship: { x: 0, y: 0 }, waypoint: { x: 10, y: 1 } };
+for (const i of instructions) {
+  position = processInstruction(i, position);
+}
+
+console.log(
+  `Manhattan distance after processing instructions is ${calculateManhattanDistance(position)}`
+);
