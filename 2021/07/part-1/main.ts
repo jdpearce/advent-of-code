96586df02fbb7f52a476e050a673dc4fc6733d9e
Jo Hanna Pearce <jo@nrwl.io>
2021-12-08 12:18:41 +0000

    egg(2021) day 7 easter egg int code

--- 2021/07/part-1/main.ts
+++ 2021/07/part-1/main.ts
@@ -1,7 +1,18 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { ProgramState, runIntCode } from '../../../2019/09/part-1/lib';
 import { calculateBestUseOfFuel } from './lib';
 
 const input = fs.readFileSync(path.join(__dirname, '../input.txt')).toString();
 
 console.log(`Best use of fuel is ${calculateBestUseOfFuel(input)}`);
+
+// EASTER EGG!
+const program = input.split('\n')[0].split(',').map(Number);
+
+const state = new ProgramState([...program]);
+state.input.push(1);
+
+runIntCode(state);
+
+console.log(state.output.map((code) => String.fromCharCode(code)).join(''));
